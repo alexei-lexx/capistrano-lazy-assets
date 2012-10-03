fac8e4b09fe07c6b87f860be48f2bd5cc99da828
Alexei Curguzchin <alexei.curguzchin@pokupon.ua>
2012-10-03 18:13:10 +0300

    experiments

--- lib/capistrano-lazy-assets.rb
+++ lib/capistrano-lazy-assets.rb
@@ -8,7 +8,7 @@ configuration.load do
   namespace :deploy do
     namespace :assets do
       task :precompile, :roles => :web, :except => { :no_release => true } do
-        logger.info 'Run lazy version of assets:precompile'
+        run 'ls -lah'
         from = source.next_revision(current_revision)
         if capture("cd #{latest_release} && #{source.local.log(from)} app/assets/ | wc -l").to_i > 0
           run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
--- lib/capistrano-lazy-assets/version.rb
+++ lib/capistrano-lazy-assets/version.rb
@@ -1,7 +1,7 @@
 module Capistrano
   module Lazy
     module Assets
-      VERSION = '0.0.3'
+      VERSION = '0.0.4'
     end
   end
 end
