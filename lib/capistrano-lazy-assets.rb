3bf59d0a86d4f489fe054fb5e194125a69a02df9
Alexei Curguzchin <alexei.curguzchin@pokupon.ua>
2012-10-03 17:56:27 +0300

    redeclare task deploy:assets:precompile

--- lib/capistrano-lazy-assets.rb
+++ lib/capistrano-lazy-assets.rb
@@ -1,21 +1,20 @@
-require "capistrano-lazy-assets/version"
-
-module Capistrano
-  module Lazy
-    module Assets
-      # Your code goes here...
-    end
-  end
-end
+require 'capistrano-lazy-assets/version'
 
 configuration = Capistrano::Configuration.respond_to?(:instance) ?
     Capistrano::Configuration.instance(:must_exist) :
     Capistrano.configuration(:must_exist)
 
 configuration.load do
-  namespace :example do
-    task :test, :roles => :app do
-      run 'ls -lah'
+  namespace :deploy do
+    namespace :assets do
+      task :precompile, :roles => :web, :except => { :no_release => true } do
+        from = source.next_revision(current_revision)
+        if capture("cd #{latest_release} && #{source.local.log(from)} app/assets/ | wc -l").to_i > 0
+          run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+        else
+          logger.info 'Skipping asset pre-compilation because there were no asset changes'
+        end
+      end
     end
   end
 end
