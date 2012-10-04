32bede554b274876f396b171777a569ccf3ef1f3
Alexei Curguzchin <alexei.curguzchin@pokupon.ua>
2012-10-04 18:31:06 +0300

    fixed loading

--- lib/capistrano-lazy-assets.rb
+++ lib/capistrano-lazy-assets.rb
@@ -1,18 +1,16 @@
 require 'capistrano-lazy-assets/version'
 
-configuration = Capistrano::Configuration.respond_to?(:instance) ?
-    Capistrano::Configuration.instance(:must_exist) :
-    Capistrano.configuration(:must_exist)
-
-configuration.load do
-  namespace :deploy do
-    namespace :assets do
-      task :precompile, :roles => :web, :except => { :no_release => true } do
-        from = source.next_revision(current_revision)
-        if capture("cd #{latest_release} && #{source.local.log(from)} app/assets/ | wc -l").to_i > 0
-          run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-        else
-          logger.info 'Skipping asset pre-compilation because there were no asset changes'
+if Capistrano::Configuration.instance
+  Capistrano::Configuration.instance.load do
+    namespace :deploy do
+      namespace :assets do
+        task :precompile, :roles => :web, :except => { :no_release => true } do
+          from = source.next_revision(current_revision)
+          if capture("cd #{latest_release} && #{source.local.log(from)} app/assets/ | wc -l").to_i > 0
+            run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+          else
+            logger.info 'Skipping asset pre-compilation because there were no asset changes'
+          end
         end
       end
     end
--- lib/capistrano-lazy-assets/version.rb
+++ lib/capistrano-lazy-assets/version.rb
@@ -1,7 +1,7 @@
 module Capistrano
   module Lazy
     module Assets
-      VERSION = '0.0.5'
+      VERSION = '0.0.6'
     end
   end
 end
