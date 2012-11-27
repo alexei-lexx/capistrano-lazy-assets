9253ab555010ce9b325f2b6a1d7c10076e9c16cf
Ernest Bursa <ernest@bzdury.pl>
2012-11-27 11:58:52 +0100

    added workarround for initial precompile (while missing current symlinkdir)

--- lib/capistrano-lazy-assets.rb
+++ lib/capistrano-lazy-assets.rb
@@ -5,8 +5,12 @@ if Capistrano::Configuration.instance
     namespace :deploy do
       namespace :assets do
         task :precompile, :roles => :web, :except => { :no_release => true } do
-          from = source.next_revision(current_revision)
-          if capture("cd #{latest_release} && #{source.local.log(from)} app/assets/ | wc -l").to_i > 0
+          begin
+            from = source.next_revision(current_revision)
+          rescue
+            errr = true
+          end
+          if errr or capture("cd #{latest_release} && #{source.local.log(from)} app/assets/ | wc -l").to_i > 0
             run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
           else
             logger.info 'Skipping asset pre-compilation because there were no asset changes'
